72aa28e76d4411d0d30370d32281b7074cb636a5
micaelasorrentino-design <micaelasorrentino@gmail.com>
2026-07-22 20:49:18 -0300

    Nueva portada elegante con header

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ const torneo = {
   cuposTotales: 32
 };
 
-const numeroInscriptas = document.getElementById("inscriptas");
-const numeroTotal = document.getElementById("total-cupos");
-const barraProgreso = document.getElementById("progreso");
-const textoCupos = document.getElementById("texto-cupos");
-const botonInscripcion = document.getElementById("boton-inscripcion");
+const numeroInscriptas =
+  document.getElementById("inscriptas");
+
+const numeroTotal =
+  document.getElementById("total-cupos");
+
+const barraProgreso =
+  document.getElementById("progreso");
+
+const textoCupos =
+  document.getElementById("texto-cupos");
+
+const botonInscripcion =
+  document.getElementById("boton-inscripcion");
+
 
 function actualizarCupos() {
   const lugaresDisponibles =
@@ -16,8 +26,11 @@ function actualizarCupos() {
   const porcentaje =
     (torneo.inscriptas / torneo.cuposTotales) * 100;
 
-  numeroInscriptas.textContent = torneo.inscriptas;
-  numeroTotal.textContent = torneo.cuposTotales;
+  numeroInscriptas.textContent =
+    torneo.inscriptas;
+
+  numeroTotal.textContent =
+    torneo.cuposTotales;
 
   barraProgreso.style.width =
     `${Math.min(porcentaje, 100)}%`;
@@ -25,17 +38,30 @@ function actualizarCupos() {
   if (lugaresDisponibles > 1) {
     textoCupos.textContent =
       `Quedan ${lugaresDisponibles} lugares`;
-  } else if (lugaresDisponibles === 1) {
-    textoCupos.textContent = "Queda 1 solo lugar";
-  } else {
-    textoCupos.textContent = "Torneo completo";
-    botonInscripcion.textContent = "Torneo completo";
+  }
+
+  if (lugaresDisponibles === 1) {
+    textoCupos.textContent =
+      "Queda un solo lugar";
+  }
+
+  if (lugaresDisponibles <= 0) {
+    textoCupos.textContent =
+      "Torneo completo";
+
+    botonInscripcion.textContent =
+      "Torneo completo";
+
     botonInscripcion.disabled = true;
   }
 }
 
+
 botonInscripcion.addEventListener("click", () => {
-  alert("Ahora vamos a conectar el formulario de inscripción 💜");
+  alert(
+    "En el próximo paso conectamos el formulario de inscripción 💜"
+  );
 });
+
 
 actualizarCupos();
