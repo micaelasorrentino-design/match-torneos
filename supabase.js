e9cd4df487556f0f087edfdc8be065de348103d3
micaelasorrentino-design <micaelasorrentino@gmail.com>
2026-08-04 20:31:52 -0300

    nuevo web match

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://ivkwgmwxbjcabuyizohq.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_iaI-H9b102a-GXHtYPGAGg_MTHC6fmT";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
