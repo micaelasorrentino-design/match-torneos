353969eb0fe4289ad5088014d73485af42f81fda
micaelasorrentino-design <micaelasorrentino@gmail.com>
2026-08-04 21:15:16 -0300

    nuevo web

--- supabase.js
+++ supabase.js
@@ -2,7 +2,13 @@ const SUPABASE_URL = "https://ivkwgmwxbjcabuyizohq.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_iaI-H9b102a-GXHtYPGAGg_MTHC6fmT";
 
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
+// Verificamos que la librería cargó
+console.log("window.supabase:", window.supabase);
+
+// Creamos el cliente y lo dejamos disponible globalmente
+window.db = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
 );
+
+console.log("Cliente:", window.db);
